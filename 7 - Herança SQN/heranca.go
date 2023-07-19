89a42df1b411c0e47bad47bf079257eb57fffa44
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2023-07-19 09:42:20 -0300

    feat: entendendo "herança" no go

---
+++ 7 - Herança SQN/heranca.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type pessoa struct {
+	nome      string
+	sobrenome string
+	idade     uint8
+	altura    uint8
+}
+
+type estudante struct {
+	pessoa
+	curso     string
+	faculdade string
+}
+
+func main() {
+	fmt.Println("Herança")
+
+	pessoa := pessoa{"Gabriel", "Gambôa", 22, 80}
+
+	fmt.Println(pessoa)
+}
