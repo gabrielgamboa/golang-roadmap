5c9a2897c4571bb4b647eef5b75e9ded45d18e76
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2023-08-15 11:00:05 -0300

    feat: search server names from specified host

--- 17 - Projeto de Linha de comando/app/app.go
+++ 17 - Projeto de Linha de comando/app/app.go
@@ -14,18 +14,26 @@ func Generate() *cli.App {
 	app.Name = "Command Line Application created in Go!"
 	app.Description = "Search ips and server names on the internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
 	//cli.Command é um array de structs dos comandos da aplicação CLI
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search ip from addresses on the internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search ip from addresses on the internet",
+			Flags:  flags,
 			Action: searchIps,
+		},
+		{
+			Name:   "server",
+			Usage:  "Search server names from addresses on the internet",
+			Flags:  flags,
+			Action: searchServerNames,
 		},
 	}
 
@@ -45,5 +53,21 @@ func searchIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println("* ", ip)
+	}
+}
+
+func searchServerNames(c *cli.Context) {
+	host := c.String("host")
+
+	servers, error := net.LookupNS(host)
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	fmt.Printf("Found %d Server Names for %s host: \n", len(servers), host)
+
+	for _, server := range servers {
+		fmt.Println("* ", server.Host)
 	}
 }
