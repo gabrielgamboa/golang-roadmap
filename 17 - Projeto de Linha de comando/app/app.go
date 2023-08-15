1fc97795e063497f9cb40694d779acc202d6525d
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2023-08-15 09:45:46 -0300

    feat: search ips from specified host in cli app

--- 17 - Projeto de Linha de comando/app/app.go
+++ 17 - Projeto de Linha de comando/app/app.go
@@ -1,12 +1,49 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
 
-//Retornar a aplicação de linha de comando para ser executada
+	"github.com/urfave/cli"
+)
+
+// Retornar a aplicação de linha de comando para ser executada
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Command Line Application created in Go!"
 	app.Description = "Search ips and server names on the internet"
 
+	//cli.Command é um array de structs dos comandos da aplicação CLI
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search ip from addresses on the internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: searchIps,
+		},
+	}
+
 	return app
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, error := net.LookupIP(host)
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	fmt.Printf("Found %d IP's for %s host: \n", len(ips), host)
+
+	for _, ip := range ips {
+		fmt.Println("* ", ip)
+	}
 }
--- 17 - Projeto de Linha de comando/main.go
+++ 17 - Projeto de Linha de comando/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"command-line-app/app"
-	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("Ponto de partida")
-
 	application := app.Generate()
 	if error := application.Run(os.Args); error != nil {
 		log.Fatal(error)
